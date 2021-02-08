5b5023f0470c82599e79195a062ba33aeb204ff2
ullu <ula.lukierska@gmail.com>
2021-02-09 00:31:17 +0100

    server

--- backend_node_mongo/server.js
+++ backend_node_mongo/server.js
@@ -1,2 +1,23 @@
 const express=require("express");
 const bodyparser=require("body-parser");
+const cors=require("cors");
+
+const app=express();
+
+var cors_org={
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(cors_org));
+app.use(bodyparser.json());
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=> {
+    res.json({message: "waiting"});
+});
+
+const PORT=process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
