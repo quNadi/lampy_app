157a2bda9c39e0d09c5422018db61892885586fb
ullu <ula.lukierska@gmail.com>
2021-02-13 01:55:54 +0100

    vuetify wr

--- front_vue/src/App.vue
+++ front_vue/src/App.vue
@@ -1,60 +1,63 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+   <v-card class="overflow-hidden">
+     <v-app-bar absolute color="#6A76AB" dark shrink-on-scroll
+ prominent fade-img-on-scroll scroll-target="#scrolling-techniques-3">
+ <template v-slot:img="{ props }">
+   <v-img v-bind="props" gradient="to top right,rgba(100,115,201,.7),rgba(25,32,72,.7)"> </v-img>
+ </template>
+ <v-app-bar-nav-icon></v-app-bar-nav-icon>
+ <v-app-bar-title> Lampy</v-app-bar-title>
+ <b-nav tabs align="center">
+   <b-nav-item>  <router-link to="/">  Home  </router-link> </b-nav-item>
+    <b-nav-item>  <router-link to="/addnew">  new </router-link> </b-nav-item>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+ </b-nav>
+
+ <v-spacer></v-spacer>
+ <v-btn icon>
+   <v-icon>mdi-magnify</v-icon>
+ </v-btn>
+ <v-btn icon> 
+   
+   <v-icon> mdi-heart</v-icon> 
+ </v-btn>
+ <template v-slot:extension>
+   <v-tabs align-with-title>
+     <v-tab>oooo</v-tab>
+     <v-tab>ooo</v-tab>
+   </v-tabs>
+ </template>
+     </v-app-bar>
+     <v-sheet id="scrolling-techniques-3" class="overflow-y-auto" max-height="600">
+    
+    <v-container fluid style="height: 1000px">
+      <div id="app">
+        <router-view/>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    </v-container >
+  
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2021</span>
+    </v-footer>
+     </v-sheet>
+    </v-card>
   </v-app>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
 
 export default {
-  name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+
 
   data: () => ({
-    //
+    drawer:null,
   }),
+  props: {
+    source:String,
+  }
 };
 </script>
---
+++ front_vue/src/components/AddNewLamp.vue
@@ -0,0 +1,76 @@
+<template>
+
+     <validation-observer
+    ref="observer"
+    v-slot="{ invalid }"
+  >
+      <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="lampy.name"
+      :counter="20"
+      label="Name"
+      required
+    ></v-text-field>
+
+        <v-text-field
+      v-model="lampy.desc"
+      :counter="50"
+      label="Description"
+      required
+    ></v-text-field>
+
+         <v-text-field
+      v-model="lampy.category"
+      :counter="15"
+      label="Category"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="lampy.person"
+      
+      label="Contact Person"
+      required
+    ></v-text-field>
+  <validation-provider
+        v-slot="{ errors }"
+        name="phoneNumber"
+        :rules="{
+          required: true,
+          digits: 9
+
+        }"
+      >
+        <v-text-field
+          v-model="phoneNumber"
+          :counter="9"
+          :error-messages="errors"
+          label="Phone Number"
+          required
+        ></v-text-field>
+      </validation-provider>
+
+    <v-btn
+      
+        class="mr-4"
+        type="submit"
+        :disabled="invalid"
+      >
+      Validate
+    </v-btn>
+
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      Reset Form
+    </v-btn>
+
+  </v-form>
+     </validation-observer>
+</template>
